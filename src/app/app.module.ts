407c5e4aae692312195c164068de21105133bc99
LeoDJ <leandro.spaeth@gmail.com>
2017-09-11 22:36:07 +0200

    add initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { ClassComponent } from './class/class.component';
+import { PupilComponent } from './pupil/pupil.component';
+import { NotesComponent } from './notes/notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherComponent,
+    ClassComponent,
+    PupilComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule
