ef3269c1a317f63a6c9a6f63332d682ce68ab4b1
raspyweather <raspyweather@gmail.com>
2017-09-16 19:21:33 +0200

    Navbar  beautification,  UserName display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import {LoginService} from "./login.service";
 import {Router} from "@angular/router";
+import {DataService} from "./data.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [LoginService]
+  providers: [LoginService, DataService]
 })
 export class AppComponent {
   title = 'app';
-  constructor(private loginService: LoginService, private router: Router) {}
+  constructor(private loginService: LoginService,private dataService:DataService, private router: Router) {}
 
   logoutVisible() {
     let url = this.router.url;
